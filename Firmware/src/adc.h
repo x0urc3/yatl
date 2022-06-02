74e5a0db4454a66bdd6d06fdb2b1b19c3d6668c5
x0urc3 <xource@gmail.com>
2022-06-02 16:14:09 +0800

    Move generic adc function to separate file

---
+++ Firmware/src/adc.h
@@ -0,0 +1,48 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define ITEMP_GAIN	    1
+#define ITEMP_OFFSET    322     // Offset for 0deg Celcius	
+#define ITEMP_SCALE     1       // AVR temp sensor: 1LSB per celcius    
+
+void initADC(void) {
+    //ADC clock input 50-200kHz
+    
+#ifdef ARDUINO_AVR_UNO
+    //UNO use 16Mhz external oscillator
+    ADCSRA |= (_BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0)); // ADC clock prescaler /128
+#else
+    ADCSRA |= (_BV(ADPS2)| _BV(ADPS1)); // ADC clock prescaler /64
+#endif 
+    ADCSRA |= _BV(ADEN);                // enable ADC
+}
+
+uint16_t getInternalTemp(void) { 
+    uint16_t temp;
+
+    ADMUX = _BV(REFS1) | _BV(REFS0);    // Internal 1.1V VRef
+    ADMUX |= _BV(MUX3);                 // Select ADC8 i.e. Temp sensor
+    _delay_ms(10);                      // Settling time after changing ADMUX
+    ADCSRA |= _BV(ADSC);                // Start conversion
+    loop_until_bit_is_clear(ADCSRA, ADSC);
+    temp = ITEMP_GAIN * (ADC - ITEMP_OFFSET);
+    return (temp);           
+}
+
+uint16_t getVcc100(void) {
+    uint16_t vcc;
+
+    ADMUX = _BV(REFS0);            // Vcc as Voltage Ref
+    ADMUX |= _BV(MUX3) | _BV(MUX2) |
+        _BV(MUX1);                 // Select internal 1.1V VRef
+    _delay_ms(2);                  // Settling time after changing ADMUX
+    ADCSRA |= _BV(ADSC);           // Start conversion
+    loop_until_bit_is_clear(ADCSRA, ADSC);
+    //vcc = 110 * 1024 / ADC;
+    vcc = 112640 / ADC;
+    return (vcc);
+}
+
+#endif //__ADC_H__
--- Firmware/src/yatl.cpp
+++ Firmware/src/yatl.cpp
@@ -9,6 +9,7 @@
  * - Switch pin should use AVR internal pullup
  * - Temperature sensor Vout connected 
  */
+#include "adc.h"
 #include "trace.h"
 #include "usart.h"
 #include <avr/io.h>
@@ -32,25 +33,10 @@
 #define SWITCHPIN   PD2   // x     |  x
 #define TEMPPIN     0xf0  // ADC0    |   23   |  A0
 
-#define ITEMP_GAIN	    1
-#define ITEMP_OFFSET    322     // Offset for 0deg Celcius	
-#define ITEMP_SCALE     1       // AVR temp sensor: 1LSB per celcius    
+
 #define ADC_VREF    50    // Vcc = 5.06
 #define TEMP_GAIN   1085  // LMT86: 10.85mV per Celcius 
 #define TEMP_OFFSET	21    // Offset for 0deg Celcius
-
-static inline void initADC(void) {
-    //ADC clock input 50-200kHz
-    
-#ifdef ARDUINO_AVR_UNO
-    //UNO use 16Mhz external oscillator
-    ADCSRA |= (_BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0)); // ADC clock prescaler /128
-#else
-    ADCSRA |= (_BV(ADPS2)| _BV(ADPS1)); // ADC clock prescaler /64
-#endif 
-    ADCSRA |= _BV(ADEN);                // enable ADC
-}
-
 
 uint16_t getTemp10(void) {
     uint16_t temp;
@@ -63,32 +49,6 @@ uint16_t getTemp10(void) {
     //T = ((ADC/1024)*VRef - TEMP_OFFSET ) / -TEMP_GAIN
     temp =  (TEMP_OFFSET*1024 - ADC*ADC_VREF)/ (TEMP_GAIN*0.01024);
     return (temp); 
-}
-
-uint16_t getInternalTemp(void) { 
-    uint16_t temp;
-
-    ADMUX = _BV(REFS1) | _BV(REFS0);    // Internal 1.1V VRef
-    ADMUX |= _BV(MUX3);                 // Select ADC8 i.e. Temp sensor
-    _delay_ms(10);                      // Settling time after changing ADMUX
-    ADCSRA |= _BV(ADSC);                // Start conversion
-    loop_until_bit_is_clear(ADCSRA, ADSC);
-    temp = ITEMP_GAIN * (ADC - ITEMP_OFFSET);
-    return (temp);           
-}
-
-uint16_t getVcc100(void) {
-    uint16_t vcc;
-
-    ADMUX = _BV(REFS0);            // Vcc as Voltage Ref
-    ADMUX |= _BV(MUX3) | _BV(MUX2) |
-        _BV(MUX1);                 // Select internal 1.1V VRef
-    _delay_ms(2);                  // Settling time after changing ADMUX
-    ADCSRA |= _BV(ADSC);           // Start conversion
-    loop_until_bit_is_clear(ADCSRA, ADSC);
-    //vcc = 110 * 1024 / ADC;
-    vcc = 112640 / ADC;
-    return (vcc);
 }
 
 void setup(void) {
