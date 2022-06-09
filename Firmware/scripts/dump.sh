805b8753718fedd76f3694b3bd4784300918a50a
x0urc3 <xource@gmail.com>
2022-06-09 13:30:55 +0800

    Add arguments and cleanup

--- Firmware/scripts/dump.sh
+++ Firmware/scripts/dump.sh
@@ -7,15 +7,54 @@ NC='\033[0m' # No Color
 PIOPATH=$HOME'/.platformio/packages/'
 AVRDUDEBIN=$PIOPATH'tool-avrdude/avrdude'
 AVRDUDECONF=$PIOPATH'tool-avrdude/avrdude.conf'
-AVRDUDECMD= $AVRDUDEBIN' -C '$AVRDUDECONF' -c stk500v1 -p m328p -P /dev/ttyUSB0 -b 19200'
+#AVRDUDECMD= $AVRDUDEBIN' -C '$AVRDUDECONF' -c stk500v1 -p m328p -P /dev/ttyUSB0 -b 19200'
 
-FNAME='./data/'`date +%d%m%Y`'.hex'
-echo -e "${GREEN}Dump EEPROM data to $FNAME${NC}"
-#pio run -e isp -t upload
-$AVRDUDEBIN -C $AVRDUDECONF -c stk500v1 -p m328p -P /dev/ttyUSB0 -b 19200 -U eeprom:r:$FNAME:i
+usage() {
+    echo "Usage: $0 [-d|-r]"
+    echo -e "\t-d\tDump eeprom to file"
+    echo -e "\t-r\tReset eeprom dirty flag"
+    exit 0;
+}
 
-echo -e "${GREEN}Remove dirty flag${NC}"
-$AVRDUDEBIN -C $AVRDUDECONF -c stk500v1 -p m328p -P /dev/ttyUSB0 -b 19200 -t<<END
+isp() {
+    echo -e "${GREEN}Upload ArduinoISP${NC}"
+    pio run -e isp -t upload
+}
+
+dump() {
+    FNAME='./data/'`date +%d%m%Y`'.hex'
+    echo -e "${GREEN}Dump EEPROM data to $FNAME${NC}"
+    $AVRDUDEBIN -C $AVRDUDECONF -c stk500v1 -p m328p -P /dev/ttyUSB0 -b 19200 -U eeprom:r:$FNAME:i
+}
+
+reset() {
+    echo -e "${GREEN}Reset dirty flag${NC}"
+    $AVRDUDEBIN -C $AVRDUDECONF -c stk500v1 -p m328p -P /dev/ttyUSB0 -b 19200 -t<<END
 write eeprom 0x00 0xff
 quit
 END
+}
+
+#Process arguments
+while getopts "dr" arg; do
+    case $arg in
+        d)
+            isp
+            dump
+            ;;
+        r)
+            isp
+            reset
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+#Default to dump and reset
+if (( $OPTIND == 1 )); then
+    isp
+    dump
+    reset
+fi
