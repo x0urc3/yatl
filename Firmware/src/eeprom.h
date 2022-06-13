eb704b84de8ce3dc2286912fc609135a329d4626
x0urc3 <xource@gmail.com>
2022-06-13 18:19:57 +0800

    Fix for storing byte data using  word-based addressing

--- Firmware/src/eeprom.h
+++ Firmware/src/eeprom.h
@@ -2,7 +2,7 @@
 #define EEPROM_H__
 #include <avr/eeprom.h>
 
-#define ROM_DIRTYFLAG 0x7a
+#define ROM_DIRTYFLAG 0x7a7a
 #ifdef ARDUINO_AVR_ATmega48
 #define ROM_SIZE 250
 #else
@@ -10,7 +10,7 @@
 #endif
 
 struct eeDataStruct {
-    uint8_t dirty;
+    uint16_t dirty;
     uint16_t dataPnt;
     uint8_t data[ROM_SIZE];
 };
@@ -20,14 +20,14 @@ struct eeDataStruct EEMEM eeData = {0};     // Required to be defined
 #define usageEEPROM() (eeprom_read_word(&eeData.dataPnt))
 
 void initEEPROM(void) {
-    uint8_t flag;
+    uint16_t flag;
     uint16_t addr;
 
-    TRACE(1, "Reading flag. DIRTY:%d\n", flag);
     TRACE(1, "DIRTYFLAG:%d\n", ROM_DIRTYFLAG);
-    flag = eeprom_read_byte(&eeData.dirty);
+    flag = eeprom_read_word(&eeData.dirty);
+    TRACE(1, "Reading flag. DIRTY:%x\n", flag);
     if (flag != ROM_DIRTYFLAG) {
-        TRACE(1, "ROM is empty. DIRTY:%d\n", flag);
+        TRACE(1, "ROM is empty. DIRTY:%x\n", flag);
         addr = eeprom_read_word(&eeData.dataPnt);
         if (addr != 0) {
             TRACE(1, "Reset addr. addr:%d\n", addr);
@@ -42,13 +42,15 @@ uint8_t storeByteEEPROM(uint8_t data) {
     TRACE(1, "Current addr:%d\n", addr);
 
     if (addr == 0) {
-        eeprom_write_byte(&eeData.dirty, ROM_DIRTYFLAG);
+        eeprom_write_word(&eeData.dirty, ROM_DIRTYFLAG);
     }
 
     if (addr < ROM_SIZE) {
         TRACE(1, "Storing data:%d\n", data);
         TRACE(1, "Storing at addr:%d\n", addr);
-        eeprom_update_byte(&eeData.data[addr], data);
+        // Word-based addressing but store byte data
+        // No bit-twiddling because of little-endian
+        eeprom_update_word(&eeData.data[addr], data);
 
         addr += 1;
         eeprom_write_word(&eeData.dataPnt, addr);
