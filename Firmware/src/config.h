8ee607f0a07b8be206748e3c01950ea7bc945313
Khairulmizam Samsudin <xource@gmail.com>
2022-07-02 10:42:14 +0800

    Move pins and configurations to config.h

---
+++ Firmware/src/config.h
@@ -0,0 +1,36 @@
+/* config.h Copyright (c) 2022 Khairulmizam Samsudin <xource@gmail.com
+ *
+ * Pins and configuration declaration
+ *
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <avr/io.h>
+
+// Reserved pins    ATMega | Phy |  Uno
+// Reset             PC6   |  1  |   reset
+// SCK               PB5   |  19 |   13
+// MISO              PB4   |  18 |   12
+// MOSI              PB3   |  17 |   11
+// SDA               PC4   |  27 |   A4
+// SCL               PC5   |  28 |   A5
+// TX                PD1   |  2  |   1
+// RX                PD0   |  3  |   0
+// ARef (100nF Gnd)  ARef  |  20 |   ARef
+
+// pinout         ATMega    | Phy  |  Uno
+#define LED1        PB0   //       |  x     |  x
+#define LED2        PB1   //       |  x     |  x
+#define LED3        PB2   //       |  x     |  x
+#define LEDPIN      PINB
+#define LEDPORT     PORTB
+#define TEMPPIN     0xf0  //   23  |  A0
+#define SWITCH      PD2   //       |  x     |  x
+#define SWITCH_PIN  PIND
+#define SWITCH_PORT PORTD
+#define SWITCH_DEBOUNCE_TIME 100
+
+#endif  // CONFIG_H_
+
--- Firmware/src/yatl.c
+++ Firmware/src/yatl.c
@@ -4,6 +4,7 @@
  *
  */
 
+#include "config.h"
 #include "trace.h"
 #include "adc.h"
 #include "interrupt.h"
@@ -11,33 +12,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/sleep.h>
-
-// Reserved pins    ATMega | Phy |  Uno
-// Reset             PC6   |  1  |   reset
-// SCK               PB5   |  19 |   13
-// MISO              PB4   |  18 |   12
-// MOSI              PB3   |  17 |   11
-// SDA               PC4   |  27 |   A4
-// SCL               PC5   |  28 |   A5
-// TX                PD1   |  2  |   1
-// RX                PD0   |  3  |   0
-// ARef (100nF Gnd)  ARef  |  20 |   ARef
-
-// pinout         ATMega    | Phy  |  Uno
-#define LED1        PB0   //       |  x     |  x
-#define LED2        PB1   //       |  x     |  x
-#define LED3        PB2   //       |  x     |  x
-#define LEDPIN      PINB
-#define LEDPORT     PORTB
-#define TEMPPIN     0xf0  //   23  |  A0
-#define SWITCH      PD2   //       |  x     |  x
-#define SWITCH_PIN  PIND
-#define SWITCH_PORT PORTD
-#define SWITCH_DEBOUNCE_TIME 100
-
-#define ADC_VREF    50      // Vcc = 5.06
-#define TEMP_GAIN   1085    // LMT86: 10.85mV per Celcius
-#define TEMP_OFFSET 21      // Offset for 0deg Celcius
 
 enum {sleep, battery, storage, logging} state;
 #define STATEMAX 4
