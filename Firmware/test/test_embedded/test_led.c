04b755f3bcaeceac0b9b33abb5d057457e9363ab
Khairulmizam Samsudin <xource@gmail.com>
2022-07-04 14:56:18 +0800

    Add test for switching on led

--- Firmware/test/test_embedded/test_led.c
+++ Firmware/test/test_embedded/test_led.c
@@ -1,15 +1,26 @@
-#include "config.h" 
+#include "config.h"
+#include <util/delay.h>
 #include <unity.h>
 
 void setUp(void) {
-    // set stuff up here
+    LEDDDR |= 0x7;
 }
 
 void tearDown(void) {
     // clean stuff up here
 }
 
+void test_led_on(void) {
+        LEDPORT |= _BV(LED1);
+        LEDPORT |= _BV(LED2);
+        LEDPORT |= _BV(LED3);
+        TEST_ASSERT_EQUAL(LEDPORT, 0x7);
+}
+
 int main( int argc, char **argv) {
+    _delay_ms(2000);
+
     UNITY_BEGIN();
+    RUN_TEST(test_led_on);
     UNITY_END();
 }
