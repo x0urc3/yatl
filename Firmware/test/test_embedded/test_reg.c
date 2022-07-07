b6acc9946201475254115c7a1048c053649cacde
Khairulmizam Samsudin <xource@gmail.com>
2022-07-07 19:48:18 +0800

    Add test initWDT() for

--- Firmware/test/test_embedded/test_reg.c
+++ Firmware/test/test_embedded/test_reg.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "interrupt.h"
 #include <util/delay.h>
 #include <unity.h>
 
@@ -11,10 +12,15 @@ void tearDown(void) {
 }
 
 void test_led_on(void) {
-        LEDPORT |= _BV(LED1);
-        LEDPORT |= _BV(LED2);
-        LEDPORT |= _BV(LED3);
-        TEST_ASSERT_EQUAL(LEDPORT, 0x7);
+    LEDPORT |= _BV(LED1);
+    LEDPORT |= _BV(LED2);
+    LEDPORT |= _BV(LED3);
+    TEST_ASSERT_EQUAL(LEDPORT, 0x7);
+}
+
+void test_wdt_prescaler_8s(void) {
+    initWDT();
+    TEST_ASSERT_EQUAL(WDTCSR, 0x21);
 }
 
 int main( int argc, char **argv) {
@@ -22,5 +28,6 @@ int main( int argc, char **argv) {
 
     UNITY_BEGIN();
     RUN_TEST(test_led_on);
+    RUN_TEST(test_wdt_prescaler_8s);
     UNITY_END();
 }
